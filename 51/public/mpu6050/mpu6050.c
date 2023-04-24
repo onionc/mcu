1a54d6c2a4f83cbb5035e0f8e2898cf7aaeec326
Chad <wsrfssx@yeah.net>
2023-04-24 09:09:05 +0800

    fix: del needless mpu6050 files

--- 51/public/mpu6050/mpu6050.c
+++
@@ -1,9 +0,0 @@
-#include "mpu6050.h"
-#include "../iic/iic.h"
-#include "../common.h" // delay1ms
-
-// IIC Á¬Ðø¶Á
-u8 mpuWriteLen(short ){
-
-}
-
--- 51/public/mpu6050/mpu6050.h
+++
@@ -1,9 +0,0 @@
-#ifndef _MPU6050_H
-#define _MPU6050_H
-
-#include <regx52.h>
-
-
-
-
-#endif
