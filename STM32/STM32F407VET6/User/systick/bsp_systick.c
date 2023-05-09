8338e0f6f5109e73614b2c395d6192ef6116ee1d
Chad <wsrfssx@yeah.net>
2023-05-09 17:39:35 +0800

    fix: 定时器

---
+++ STM32/STM32F407VET6/User/systick/bsp_systick.c
@@ -0,0 +1,4 @@
+#include "./systick/bsp_systick.h"
+
+__STATIC_INLINE uint32_t SysTick_Cfg(uint32_t ticks){
+}
---
+++ STM32/STM32F407VET6/User/systick/bsp_systick.h
@@ -0,0 +1,6 @@
+#ifndef __BSP_SYSTICK_H
+#define __BSP_SYSTICK_H
+
+#include "stm32f4xx.h"
+
+#endif
