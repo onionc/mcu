a3660ffed56118f892b49c4aa66b903c129baf45
Chad <wsrfssx@yeah.net>
2023-07-07 17:37:19 +0800

    fix: 实际的功能，5ms发送串口数据

---
+++ STM32/STM32F407VET6/Project/B001.5ms发送串口数据/main.c
@@ -0,0 +1,79 @@
+// 1.手动接收处理
+#include "stm32f4xx.h"
+#include "./led/bsp_led.h"
+#include "./usart/bsp_usart.h"
+
+int main(){
+    char chTemp;
+    u8 rbuf[256];
+    
+    // 初始化usart
+    Usart3_Cfg(9600);
+    
+    printf("初始化成功");
+
+
+    while(1){
+        //直接接收数据，不使用中断源接收
+        
+        if(scanf("%s", rbuf)){
+            printf("%s", rbuf);
+        }
+        
+        //chTemp = getchar();
+        //printf("%c",chTemp);
+    }
+    
+    return 0;
+}
+
+/*
+// 2.使用中断接收处理
+#include "stm32f4xx.h"
+#include "./led/bsp_led.h"
+#include "./usart/bsp_usart_interrupt.h"
+
+
+
+int main(){
+    
+    u8 rxBuf[256]={0};
+    u32 rxCount = 0;
+    
+    
+    // 初始化usart
+    Usart_Cfg(9600);
+    
+    printf("初始化成功");
+
+
+    while(1){
+        
+        if(RxFlag){
+            if(rxCount < sizeof(rxBuf)){
+                rxBuf[rxCount++] = chTemp;
+            }else{
+                rxCount = 0;
+            }
+            
+            if(chTemp==0x0a){
+                Usart_SendStr(USARTx, "received: ", 0);
+                Usart_SendStr(USARTx, rxBuf, rxCount);
+                
+                rxCount = 0;
+            }
+            RxFlag = 0;
+        }
+        
+        
+       
+    }
+    
+    return 0;
+}
+*/
+
+
+ 
+
+        
