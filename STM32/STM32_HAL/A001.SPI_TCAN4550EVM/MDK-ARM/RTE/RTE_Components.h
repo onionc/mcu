05f07f9c5ef3e3edaf82f2abc26d5ac86bfb153e
Chad <wsrfssx@yeah.net>
2023-12-07 10:10:26 +0800

    fix: 创建hal库，使用SPI读取CANFD板卡

---
+++ STM32/STM32_HAL/A001.SPI_TCAN4550EVM/MDK-ARM/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'A001.SPI_TCAN4550EVM' 
+ * Target:  'A001.SPI_TCAN4550EVM' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
