77f74b0f4722e74ada6c52ae7da364588d94956a
Chad <wsrfssx@yeah.net>
2023-12-21 14:08:30 +0800

    feat: 新建项目

---
+++ STM32/STM32_HAL/A002.I2C_MPU6050/MDK-ARM/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'A002.I2C_MPU6050' 
+ * Target:  'A002.I2C_MPU6050' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
